9600bcc77ba9a79a5e061e7064c501d1de5b90b1
omarouhra <omarmaroc26@gmail.com>
2023-11-01 16:43:38 +0100

    feat: Add navbar component to index layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen ">
+      <Navbar />
       <h1 className="text-2xl font-semibold">Welcome to dashboard app</h1>
     </div>
   );
