7a27de94b8d7eaa22d257d0a3ca447e5c6fa20c1
amine alaoui <omari1amineco@gmail.com>
2023-11-02 18:08:37 +0100

    feat: Add focus style to inbox icon button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className='bg-gray-900 w-fit h-screen flex'>
       <div className='text-white flex flex-col mt-40'>
-        <button className='p-6'>
+        <button className='p-6 focus:border-l-4 focus:border-violet-500 focus:bg-gradient-to-r focus:from-violet-950/40 focus:to-gray-900'>
         <InboxIcon />
         </button>
       </div>
