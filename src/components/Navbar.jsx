8473402560d3a5b3d1ef12a0fa00e684cd94aa05
amine alaoui <omari1amineco@gmail.com>
2023-11-07 13:38:14 +0100

    feat: make Button component dynamic with all icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import Button from "./Button";
 import InboxIcon from "@/icons/InboxIcon";
-
+import Calender from "@/icons/Calender";
+import Users from "@/icons/Users";
+import VideoCamera from "@/icons/VideoCamera";
+import DocumentDuplicate from "@/icons/DocumentDuplicate";
+import Camera from "@/icons/Camera";
 
 const Navbar = () => {
   const BUTTON_ARRAY = [
     InboxIcon,
+    Calender,
+    Users,
+    VideoCamera,
+    DocumentDuplicate,
+    Camera,
   ];
 
   return (
     <nav className="bg-gray-900 w-[100px] h-screen">
-      <div className="text-white flex flex-col mt-40">
+      <div className="text-white flex flex-col mt-20">
         {BUTTON_ARRAY.map((Icon, index) => (
           <Button key={index} icon={<Icon />} />
         ))}
