447679db946661fe4c2171a397258066a91fa4ca
amine alaoui <omari1amineco@gmail.com>
2023-11-07 13:30:46 +0100

    feat: make Button component reusable

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,9 @@
-import InboxIcon from "@/icons/InboxIcon";
 import React from "react";
 
-const Button = (icon) => {
+const Button = ({icon}) => {
   return (
     <button className="group relative p-6 focus:bg-gradient-to-r focus:from-violet-950/50 focus:to-gray-900">
-      <InboxIcon />
+      {icon}
       <div className="absolute left-0 top-0 h-full w-2 group-focus:bg-violet-500/90"></div>
     </button>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Button from "./Button";
+import InboxIcon from "@/icons/InboxIcon";
 
-const BUTTON_ARRAY = [];
 
 const Navbar = () => {
+  const BUTTON_ARRAY = [
+    InboxIcon,
+  ];
+
   return (
     <nav className="bg-gray-900 w-[100px] h-screen">
       <div className="text-white flex flex-col mt-40">
-        <Button />
+        {BUTTON_ARRAY.map((Icon, index) => (
+          <Button key={index} icon={<Icon />} />
+        ))}
       </div>
     </nav>
   );
