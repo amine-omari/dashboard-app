1fd4aff2968f8371a40614904825b47527ac21c2
amine alaoui <omari1amineco@gmail.com>
2023-11-09 20:23:44 +0100

    fix: Search icon

--- src/icons/Search.jsx
+++ src/icons/Search.jsx
@@ -8,7 +8,7 @@ const Search = () => {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="w-6 h-6"
+      className="w-6 h-6 text-gray-500"
     >
       <path
         strokeLinecap="round"
