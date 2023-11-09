9546da840a141e360338d2e1c2167c49fe86c624
amine alaoui <omari1amineco@gmail.com>
2023-11-09 20:52:51 +0100

    fix: Search bar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,13 +1,16 @@
+import Bell from "@/icons/Bell";
 import Search from "@/icons/Search";
 import React from "react";
 
 const Topbar = () => {
   return (
-    <div className="flex flex-row h-20 bg-gray-900 w-full border border-gray-700">
-      <div className="ml-10 text-white py-7 flex gap-x-4">
-        <Search />
+    <div className="flex justify-between h-20 bg-gray-900 w-full border border-gray-700">
+      <div className="ml-10 py-7 flex gap-x-4">
+        <div className="text-gray-400">
+          <Search />
+        </div>
         <input
-          className="bg-transparent w-[260px] focus:outline-none placeholder:text-gray-500"
+          className="bg-transparent w-[260px] focus:outline-none placeholder:text-gray-500 text-white"
           type="search"
           placeholder="Search for activies, people, events"
         />
--- src/icons/Search.jsx
+++ src/icons/Search.jsx
@@ -8,7 +8,7 @@ const Search = () => {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="w-6 h-6 text-gray-500"
+      className="w-6 h-6"
     >
       <path
         strokeLinecap="round"
