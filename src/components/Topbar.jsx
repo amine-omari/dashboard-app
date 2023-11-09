f632ef4a936ae2cf17c0229de56296247cfe33ed
amine alaoui <omari1amineco@gmail.com>
2023-11-09 19:35:11 +0100

    feta: Add search bar to Topbar component

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,8 +1,18 @@
+import Search from "@/icons/Search";
 import React from "react";
 
 const Topbar = () => {
   return (
-    <div></div>
+    <div className="flex flex-row h-20 bg-gray-900 w-full border border-gray-700">
+      <div className="ml-10 text-white py-7 flex gap-x-4">
+        <Search />
+        <input
+          className="bg-transparent w-[260px] focus:outline-none placeholder:text-gray-500"
+          type="search"
+          placeholder="Search for activies, people, events"
+        />
+      </div>
+    </div>
   );
 };
 
