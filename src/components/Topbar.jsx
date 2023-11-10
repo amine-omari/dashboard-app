21118bb3d79accffc9bd51bd232c7b32ea15b9f5
amine alaoui <omari1amineco@gmail.com>
2023-11-10 12:36:56 +0100

    feat: notification and profile bar in Topbar component

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,3 +1,5 @@
+import Bell from "@/icons/Bell";
+import ChevronDown from "@/icons/ChevronDown";
 import Search from "@/icons/Search";
 import React from "react";
 
@@ -13,6 +15,27 @@ const Topbar = () => {
           type="search"
           placeholder="Search activies, people, events"
         />
+      </div>
+      <div className="flex items-center gap-x-6 mr-10">
+        <div className="relative text-gray-400">
+          <span className="absolute h-1.5 w-1.5 bg-red-500 rounded-full right-1 top-0"></span>
+          <Bell />
+        </div>
+        <div className="text-white flex items-center gap-x-3">
+          <div>
+            <img
+              src="https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsX29mZmljZV8yNF9pc29sYXRlZF8zZF9pbGx1c3RyYXRpb25fb2ZfcHJvZmlsZV9pY29uX2ZhY18zOWE2ZTBiMC0zNTg3LTQ4OTUtYjgyNi01MGUzYmQ4MDA3MTMucG5n.png"
+              alt="profile image"
+              className="w-8 h-8 rounded-full"
+            />
+          </div>
+          <div className="flex items-center gap-x-3">
+            <p>Dominik Tyka</p>
+            <span className="text-gray-400">
+              <ChevronDown />
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
