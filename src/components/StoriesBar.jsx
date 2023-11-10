6f448e38c016bad2b8a45a592d291a32b346ff05
amine alaoui <omari1amineco@gmail.com>
2023-11-10 19:45:35 +0100

    feat: Add Plus icon to StoriesBar component

--- src/components/StoriesBar.jsx
+++ src/components/StoriesBar.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import Plus from "@/icons/plus";
+import React from "react";
 
 const StoriesBar = () => {
   return (
     <div>
+      <div>
+        <Plus />
+        <p>My Story</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StoriesBar
+export default StoriesBar;
