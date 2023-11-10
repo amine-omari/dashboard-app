9006df1de2348de9be2ce910a9267fd08f620809
amine alaoui <omari1amineco@gmail.com>
2023-11-10 11:19:26 +0100

    refactor: Search bar in Topbar component

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,19 +1,18 @@
-import Bell from "@/icons/Bell";
 import Search from "@/icons/Search";
 import React from "react";
 
 const Topbar = () => {
   return (
-    <div className="flex justify-between h-20 bg-gray-900 w-full border border-gray-700">
-      <div className="ml-10 py-7 flex gap-x-4">
-        <div className="text-gray-400">
-          <Search />
-        </div>
-        <input
-          className="bg-transparent w-[260px] focus:outline-none placeholder:text-gray-500 text-white"
-          type="search"
-          placeholder="Search for activies, people, events"
-        />
+    <div className="flex items-center justify-between h-20 bg-gray-900 w-full border border-gray-800">
+      <div className="flex gap-x-4 ml-10">
+          <div className="text-white">
+            <Search />
+          </div>
+          <input
+            className="w-[250px] bg-transparent outline-none placeholder:text-gray-600 text-white"
+            type="search"
+            placeholder="Search activies, people, events"
+          />
       </div>
     </div>
   );
