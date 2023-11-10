57f885b82fb299490d8053e7a189a5018bdb9b30
amine alaoui <omari1amineco@gmail.com>
2023-11-10 18:04:28 +0100

    feat: Add new StoriesBar component

---
+++ src/components/MessagesStoriesBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MessagesStoriesBar = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default MessagesStoriesBar
---
+++ src/components/StoriesBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StoriesBar = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default StoriesBar
