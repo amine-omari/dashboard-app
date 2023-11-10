3a73501ec43ef89dd1214ff0c5e49f04be7afcc4
amine alaoui <omari1amineco@gmail.com>
2023-11-10 17:34:09 +0100

    fix: Topbar component

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -5,11 +5,11 @@ import React from "react";
 
 const Topbar = () => {
   return (
-    <div className="flex items-center justify-between h-20 bg-gray-900 w-full border border-gray-800">
-      <div className="flex gap-x-4 ml-10">
-        <div className="text-gray-400">
+    <div className="flex justify-between h-20 bg-gray-900 w-full border border-gray-800">
+      <div className="flex items-center gap-x-4 ml-10">
+        <span className="text-gray-400 w-6">
           <Search />
-        </div>
+        </span>
         <input
           className="w-[250px] bg-transparent outline-none placeholder:text-gray-600 text-white"
           type="search"
@@ -21,7 +21,7 @@ const Topbar = () => {
           <span className="absolute h-1.5 w-1.5 bg-red-500 rounded-full right-1 top-0"></span>
           <Bell />
         </div>
-        <div className="text-white flex items-center gap-x-3">
+        <div className="text-white flex gap-x-3">
           <div>
             <img
               src="https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsX29mZmljZV8yNF9pc29sYXRlZF8zZF9pbGx1c3RyYXRpb25fb2ZfcHJvZmlsZV9pY29uX2ZhY18zOWE2ZTBiMC0zNTg3LTQ4OTUtYjgyNi01MGUzYmQ4MDA3MTMucG5n.png"
@@ -31,7 +31,7 @@ const Topbar = () => {
           </div>
           <div className="flex items-center gap-x-3">
             <p>Dominik Tyka</p>
-            <span className="text-gray-400">
+            <span className="text-gray-400 w-3">
               <ChevronDown />
             </span>
           </div>
