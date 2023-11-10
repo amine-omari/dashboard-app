adc2f3d2fbbeff91f9a85f854216f08bc8496035
amine alaoui <omari1amineco@gmail.com>
2023-11-10 12:11:52 +0100

    fix: Search bar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -5,14 +5,14 @@ const Topbar = () => {
   return (
     <div className="flex items-center justify-between h-20 bg-gray-900 w-full border border-gray-800">
       <div className="flex gap-x-4 ml-10">
-          <div className="text-white">
-            <Search />
-          </div>
-          <input
-            className="w-[250px] bg-transparent outline-none placeholder:text-gray-600 text-white"
-            type="search"
-            placeholder="Search activies, people, events"
-          />
+        <div className="text-gray-400">
+          <Search />
+        </div>
+        <input
+          className="w-[250px] bg-transparent outline-none placeholder:text-gray-600 text-white"
+          type="search"
+          placeholder="Search activies, people, events"
+        />
       </div>
     </div>
   );
