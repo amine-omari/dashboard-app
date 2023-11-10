6de1e18132037c13b5a7d54beb183ddb16d83714
amine alaoui <omari1amineco@gmail.com>
2023-11-10 21:02:01 +0100

    feat: Add story button to the StoriesBar component

--- src/components/StoriesBar.jsx
+++ src/components/StoriesBar.jsx
@@ -3,11 +3,13 @@ import React from "react";
 
 const StoriesBar = () => {
   return (
-    <div>
-      <div>
-        <Plus />
+    <div className="p-5">
+      <button className="flex flex-col text-gray-200 font-semibold text-sm">
+        <span className="border border-gray-700 p-4 rounded-full text-gray-400">
+          <Plus />
+        </span>
         <p>My Story</p>
-      </div>
+      </button>
     </div>
   );
 };
