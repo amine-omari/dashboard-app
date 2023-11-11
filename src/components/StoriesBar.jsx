da719eb9e292eb8bf7d6396d674d8eac39029160
amine alaoui <omari1amineco@gmail.com>
2023-11-11 01:07:47 +0100

    feat: make the StoryPreview button reusable and dynamic in StoriesBar component

--- src/components/StoriesBar.jsx
+++ src/components/StoriesBar.jsx
@@ -1,15 +1,37 @@
 import Plus from "@/icons/plus";
 import React from "react";
+import StoryPreview from "./StoryPreview";
 
 const StoriesBar = () => {
+  const STORY_IMGS_data = [
+    {
+      id: 1,
+      src: "/imgs/StoryImg1.jpg",
+      text: "Arthur M.",
+    },
+    {
+      id: 2,
+      src: "/imgs/StoryImg2.jpg",
+      text: "Mia K.",
+    },
+    {
+      id: 3,
+      src: "/imgs/StoryImg3.jpg",
+      text: "Kate R.",
+    },
+  ];
+
   return (
-    <div className="p-5">
+    <div className="flex gap-x-6 p-5">
       <button className="flex flex-col text-gray-200 font-semibold text-sm">
         <span className="border border-gray-700 p-4 rounded-full text-gray-400">
           <Plus />
         </span>
         <p>My Story</p>
       </button>
+      {STORY_IMGS_data.map((item) => (
+        <StoryPreview key={item.id} src={item.src} text={item.text} />
+      ))}
     </div>
   );
 };
