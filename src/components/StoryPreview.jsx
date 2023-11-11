b7e7dab94602511e6c560a40791e676e3b9c4a72
amine alaoui <omari1amineco@gmail.com>
2023-11-11 11:29:27 +0100

    fix: StoryPreview component

--- src/components/StoryPreview.jsx
+++ src/components/StoryPreview.jsx
@@ -4,7 +4,7 @@ const StoryPreview = ({ src, text }) => {
   return (
     <button className="flex flex-col items-center text-gray-200 font-semibold text-sm">
       <span>
-        <img src={src} className="w-[58px] h-[58px] rounded-full" />
+        <img src={src} className="min-w-[58px] min-h-[58px] rounded-full" />
       </span>
       <p>{text}</p>
     </button>
