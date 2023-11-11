21782be10acd70f4b33d1442ed5896ea5b54909b
amine alaoui <omari1amineco@gmail.com>
2023-11-11 17:33:47 +0100

    fix: StoriesBar component

--- src/components/StoriesBar.jsx
+++ src/components/StoriesBar.jsx
@@ -7,27 +7,31 @@ const StoriesBar = () => {
     {
       id: 1,
       src: "/imgs/StoryImg1.jpg",
+      alt: "profile-img1",
       text: "Arthur M.",
     },
     {
       id: 2,
       src: "/imgs/StoryImg2.jpg",
+      alt: "profile-img2",
       text: "Jake K.",
     },
     {
       id: 3,
       src: "/imgs/StoryImg3.jpg",
+      alt: "profile-img3",
       text: "Kate R.",
     },
     {
       id: 4,
       src: "/imgs/StoryImg4.jpg",
+      alt: "profile-img4",
       text: "Jack M.",
     },
   ];
 
   return (
-    <div className="overflow-auto whitespace-nowrap flex justify-between gap-x-6 p-5">
+    <div className="overflow-auto whitespace-nowrap flex gap-x-6 p-5">
       <button className="flex flex-col text-gray-200 font-semibold text-sm">
         <span className="border border-gray-700 p-4 rounded-full text-gray-400">
           <Plus />
@@ -35,7 +39,7 @@ const StoriesBar = () => {
         <p>My Story</p>
       </button>
       {STORY_IMGS_data.map((item) => (
-        <StoryPreview key={item.id} src={item.src} text={item.text} />
+        <StoryPreview key={item.id} src={item.src} alt={item.alt} text={item.text} />
       ))}
     </div>
   );
