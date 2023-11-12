8d315e99fa8db507eb6e190c45855751d1720d6d
amine alaoui <omari1amineco@gmail.com>
2023-11-12 22:13:57 +0100

    feat: make MessagePreview component reusable for  MessagesBar component

--- src/components/MessagePreview.jsx
+++ src/components/MessagePreview.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import Seen from "@/icons/seen";
+import React from "react";
 
-const MessagePreview = () => {
+const MessagePreview = ({
+  src,
+  name,
+  lastMessage,
+  time,
+  seen,
+  notification,
+}) => {
   return (
-    <div></div>
-  )
-}
+    <button className="p-5 flex justify-between w-full">
+      <span>
+        <img
+          src={src}
+          alt="profile pic"
+          className="max-w-12 max-h-12 rounded-full"
+        />
+      </span>
+      <div className="flex flex-col items-start">
+        <h3 className="text-gray-200">{name}</h3>
+        <p className="text-gray-500 text-sm">{lastMessage}</p>
+      </div>
+      <div className="text-sm mt-2 flex flex-col items-center space-y-0.5">
+        <time className="text-gray-500 text-xs">{time}</time>
+        <p className="text-white bg-violet-500 px-1.5 rounded-full">
+          {seen && <Seen />}
+          {notification && <Notification />}
+        </p>
+      </div>
+    </button>
+  );
+};
 
-export default MessagePreview
+export default MessagePreview;
