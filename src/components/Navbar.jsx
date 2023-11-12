26e627d1d14d6d9b4876c898e77b5f172c7a85eb
amine alaoui <omari1amineco@gmail.com>
2023-11-12 18:44:07 +0100

    fix: border color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-gray-900 w-20 min-h-screen">
-      <div className="text-center pt-4 pb-6 w-full border-b border-gray-800 font-semibold text-white text-4xl">
+      <div className="text-center pt-4 pb-6 w-full border-b border-gray-700/60 font-semibold text-white text-4xl">
         c<span className="text-violet-500/90">.</span>
       </div>
       <div className="text-white flex flex-col mt-28">
