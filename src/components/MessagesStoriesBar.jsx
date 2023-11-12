7b6e6790e224ccbb16f48b106e9201d1722b1cde
amine alaoui <omari1amineco@gmail.com>
2023-11-12 17:49:29 +0100

    fix: import MessagesBar component to MessagesStoriesBar component instead of MessagePreview component

--- src/components/MessagesStoriesBar.jsx
+++ src/components/MessagesStoriesBar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import StoriesBar from "./StoriesBar";
-import MessagePreview from "./MessagePreview";
+import MessagesBar from "./MessagesBar";
 
 const MessagesStoriesBar = () => {
   return (
     <div className="bg-gray-900/95 flex flex-col min-h-screen w-[370px]">
       <StoriesBar />
-      <MessagePreview />
+      <MessagesBar />
     </div>
   );
 };
