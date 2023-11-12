ba57840acf4d106ac7e3c2b0f195ad240777f541
amine alaoui <omari1amineco@gmail.com>
2023-11-12 21:28:24 +0100

    feat: Add firt preview message button

--- src/components/MessagesBar.jsx
+++ src/components/MessagesBar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const MessagesBar = () => {
   return (
-    <div></div>
-  )
-}
+    <div>
+      <button className="p-5 flex justify-between w-full">
+        <span>
+          <img
+            src="/imgs/ProfilePic1.jpg"
+            alt="profile pic"
+            className="max-w-12 max-h-12 rounded-full"
+          />
+        </span>
+        <div className="flex flex-col items-start">
+          <h3 className="text-gray-200">Arthur Maliza</h3>
+          <p className="text-gray-500 text-sm">
+            It is a long established fact that a...
+          </p>
+        </div>
+        <div className="text-sm mt-1.5 flex flex-col items-center">
+          <time className="text-gray-500">6:03</time>
+          <p className="text-white bg-violet-500 px-1.5 rounded-full">1</p>
+        </div>
+      </button>
+    </div>
+  );
+};
 
-export default MessagesBar
+export default MessagesBar;
