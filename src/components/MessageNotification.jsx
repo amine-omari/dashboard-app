429b54b5893666c86ae17b80cd72ca343d6103a4
amine alaoui <omari1amineco@gmail.com>
2023-11-12 22:49:20 +0100

    fix: Message Notification component name

--- src/components/MessageNotification.jsx
+++ src/components/MessageNotification.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MessageNotification = () => {
   return (
     <p className="text-white bg-violet-500 px-1.5 rounded-full">1</p>
-  )
+  );
 }
 
 export default MessageNotification
