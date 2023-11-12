bdfd6bff55be5951c9a22e40623bc92df64f1e7c
amine alaoui <omari1amineco@gmail.com>
2023-11-12 21:27:32 +0100

    feat: Add new Seen icon

---
+++ src/icons/Seen.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Seen = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="green"
+      className="w-4 h-4"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4.5 12.75l6 6 9-13.5"
+      />
+    </svg>
+  );
+};
+
+export default Seen;
