61def53d7af6f79b32522694b79aa83c3bb4ce3c
amine alaoui <omari1amineco@gmail.com>
2023-11-12 16:50:36 +0100

    fix: width of MessagesStoriesBar component

--- src/components/MessagesStoriesBar.jsx
+++ src/components/MessagesStoriesBar.jsx
@@ -3,7 +3,7 @@ import StoriesBar from "./StoriesBar";
 
 const MessagesStoriesBar = () => {
   return (
-    <div className="bg-gray-900/95 flex flex-col min-h-screen w-[350px]">
+    <div className="bg-gray-900/95 flex flex-col min-h-screen w-[370px]">
       <StoriesBar />
     </div>
   );
