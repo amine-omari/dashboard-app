d8a36614b5f3a005e9cb637d052edd63fd5118f3
amine alaoui <omari1amineco@gmail.com>
2023-11-14 21:51:50 +0100

    feat: import ChatBarSection component to MainChatBar component

--- src/components/MainChatBar.jsx
+++ src/components/MainChatBar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ChatTopbar from "./ChatTopbar";
+import ChatBarSection from "./ChatBarSection";
 
 const MainChatBar = () => {
   return (
     <div className="bg-gray-900 w-[650px] flex flex-col">
       <ChatTopbar />
+      <ChatBarSection/>
     </div>
   );
 };
