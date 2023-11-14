26bb518634d96e6e2bd92013ce0c1a98fa3e050c
amine alaoui <omari1amineco@gmail.com>
2023-11-14 22:50:04 +0100

    fix: Seen icon

--- src/icons/Seen.jsx
+++ src/icons/Seen.jsx
@@ -8,7 +8,7 @@ const Seen = () => {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="green"
-      className="w-4 h-4"
+      className="w-3 h-3"
     >
       <path
         strokeLinecap="round"
