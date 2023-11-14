9ce48f8d571484e186718c0f24566e8cef570374
amine alaoui <omari1amineco@gmail.com>
2023-11-14 12:42:30 +0100

    feat: Add profile img to ChatTopbar

--- src/components/ChatTopbar.jsx
+++ src/components/ChatTopbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const ChatTopbar = () => {
   return (
-    <div>ChatTopbar</div>
-  )
-}
+    <div className="h-[139px] border-b border-gray-700/60 flex items-center px-8">
+      <div className="flex gap-x-4 items-center">
+        <img
+          src="/imgs/ProfilePic2.jpg"
+          alt="ProfilePic"
+          className="w-12 h-12 rounded-full"
+        />
+        <div>
+          <h2 className="text-white text-2xl">Michael Dias</h2>
+          <p className="text-gray-500 text-xs">Offline</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ChatTopbar
+export default ChatTopbar;
