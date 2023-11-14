bcd2cf4c6d88dd3b9a67a709c51d280ef4e43fb1
amine alaoui <omari1amineco@gmail.com>
2023-11-14 20:22:37 +0100

    fix: ChevronRight icon

--- src/icons/ChevronRight.jsx
+++ src/icons/ChevronRight.jsx
@@ -8,7 +8,7 @@ const ChevronRight = () => {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="w-4 h-4"
+      className="w-3 h-3"
     >
       <path
         strokeLinecap="round"
