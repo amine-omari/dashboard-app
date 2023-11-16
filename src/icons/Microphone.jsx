200f3c431fbc1e61b0c9d0d23477253e28cc8a05
amine alaoui <omari1amineco@gmail.com>
2023-11-16 17:44:34 +0100

    fix: Microphone icon

--- src/icons/Microphone.jsx
+++ src/icons/Microphone.jsx
@@ -8,7 +8,7 @@ const Microphone = () => {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="w-6 h-6"
+      className="w-5 h-5"
     >
       <path
         strokeLinecap="round"
