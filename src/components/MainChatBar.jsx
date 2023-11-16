42330e86303d4e1640fcd675e2eaddc619609429
amine alaoui <omari1amineco@gmail.com>
2023-11-16 19:11:50 +0100

    feat: import SendMessageBar component to MainChatBar component

--- src/components/MainChatBar.jsx
+++ src/components/MainChatBar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ChatTopbar from "./ChatTopbar";
 import ChatBarSection from "./ChatBarSection";
+import SendMessageBar from "./SendMessageBar";
 
 const MainChatBar = () => {
   return (
     <div className="bg-gray-900 w-[650px] flex flex-col border-x border-gray-700/60">
       <ChatTopbar />
-      <ChatBarSection/>
+      <ChatBarSection />
+      <SendMessageBar />
     </div>
   );
 };
