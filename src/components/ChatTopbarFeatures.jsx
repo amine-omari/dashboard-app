de143a90993ab5d51c5492437a08d0c466b33023
amine alaoui <omari1amineco@gmail.com>
2023-11-16 19:09:23 +0100

    fix: ChatTopbarFeatures component

--- src/components/ChatTopbarFeatures.jsx
+++ src/components/ChatTopbarFeatures.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const ChatTopbarFeatures = ({icon, textcolor}) => {
+const ChatTopbarFeatures = ({icon, textcolor, hovertext}) => {
   return (
-    <button className={`border border-gray-700/60 p-3 rounded-full ${textcolor}`}>
+    <button className={`border border-gray-700/60 p-3 rounded-full hover:bg-gray-800 ${textcolor} ${hovertext}`}>
       {icon}
     </button>
   );
