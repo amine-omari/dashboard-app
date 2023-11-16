8bb246602aa20376d401a2a03d9f10d7cab3356d
amine alaoui <omari1amineco@gmail.com>
2023-11-16 17:37:48 +0100

    fix: Link icon file name

--- src/icons/Linked.jsx
+++ src/icons/Linked.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Link = () => {
+const Linked = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -19,4 +19,4 @@ const Link = () => {
   );
 }
 
-export default Link
+export default Linked
