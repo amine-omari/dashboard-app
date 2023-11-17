1eeb04e868629b18f3e175a6397048740f879763
amine alaoui <omari1amineco@gmail.com>
2023-11-18 00:30:18 +0100

    feat: import ChatProfilePic component to Message component

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import ChatProfilePic from "./ChatProfilePic";
 
-const Messages = ({ children }) => {
+const Messages = ({ img, children, rounded, rightleft, time }) => {
   return (
-    <div>
-      <h2>hello world</h2>
-      {children}
+    <div className="flex gap-x-3">
+      {img && <ChatProfilePic />}
+      <div
+        className={`relative bg-gray-800 p-3 border-2 space-y-4 border-gray-700/60 ${rounded}`}
+      >
+        {children}
+        <time className={`absolute text-xs text-gray-500 ${rightleft}`}>
+          {time}
+        </time>
+      </div>
     </div>
   );
 };
