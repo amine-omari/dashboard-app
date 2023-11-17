4827081fbb55b46f362ee15b6436f6a7bf02fbca
amine alaoui <omari1amineco@gmail.com>
2023-11-17 18:43:11 +0100

    feat: add width and height to ChatProfilePic component

--- src/components/ChatProfilePic.jsx
+++ src/components/ChatProfilePic.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const ChatProfilePic = () => {
   return (
-    <img src="/imgs/ProfilePic2.jpg" alt="ProfilePic" />
-  )
-}
+    <img
+      src="/imgs/ProfilePic2.jpg"
+      alt="ProfilePic"
+      className="w-10 h-10 rounded-full"
+    />
+  );
+};
 
-export default ChatProfilePic
+export default ChatProfilePic;
