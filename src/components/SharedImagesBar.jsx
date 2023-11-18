10e94be8da7557a82f58ad2df621e7ad1f1b1325
amine alaoui <omari1amineco@gmail.com>
2023-11-18 21:21:36 +0100

    feat: import  ImgsSharedContainer component to  SharedImagesBar component

--- src/components/SharedImagesBar.jsx
+++ src/components/SharedImagesBar.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import ImgsSharedContainer from "./ImgsSharedContainer";
 
 const SharedImagesBar = () => {
-  return (
-    <div>SharedImagesBar</div>
-  )
-}
+  const SHARED_IMGS_DATA = [
+    {
+      id: 1,
+      src: "/imgs/GradientImg1.jpg",
+    },
+    {
+      id: 1,
+      src: "/imgs/GradientImg2.jpg",
+    },
+    {
+      id: 1,
+      src: "/imgs/GradientImg3.jpg",
+    },
+    {
+      id: 1,
+      src: "/imgs/GradientImg4.jpg",
+    },
+    {
+      id: 1,
+      src: "/imgs/GradientImg1.jpg",
+    },
+  ];
 
-export default SharedImagesBar
+  return (
+    <div className="space-y-3">
+      <div className="flex justify-between">
+        <h2 className="text-white">Shared Images</h2>
+        <button className="text-violet-500 text-sm">Show all</button>
+      </div>
+      <div className="flex overflow-auto scrollbar-thin scrollbar-thumb-gray-700/30 py-2 gap-x-2">
+        {SHARED_IMGS_DATA.map(({ src }) => (
+          <ImgsSharedContainer src={src} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SharedImagesBar;
