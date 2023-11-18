a1ccdeac314c3862badb41d901f9d4eb22a4d560
amine alaoui <omari1amineco@gmail.com>
2023-11-18 20:10:36 +0100

    feat: Add width to MainSharedBar component

--- src/components/MainSharedBar.jsx
+++ src/components/MainSharedBar.jsx
@@ -3,7 +3,7 @@ import SharedImagesBar from "./SharedImagesBar";
 
 const MainSharedBar = () => {
   return (
-    <div className="bg-gray-900 p-5">
+    <div className="bg-gray-900 p-5 w-[25%]">
       <SharedImagesBar />
     </div>
   );
