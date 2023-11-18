59e4727c88f0cd7503d7ae8accdef9ef970c12b1
amine alaoui <omari1amineco@gmail.com>
2023-11-18 20:33:43 +0100

    refactor: change ImgsSharedBar component to ImgsSharedContainer component

--- src/components/ImgsSharedBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ImgsSharedBar = () => {
-  return (
-    <div>ImgsSharedBar</div>
-  )
-}
-
-export default ImgsSharedBar
---
+++ src/components/ImgsSharedContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ImgsSharedContainer = () => {
+  return (
+    <div>ImgsSharedContainer</div>
+  )
+}
+
+export default ImgsSharedContainer
