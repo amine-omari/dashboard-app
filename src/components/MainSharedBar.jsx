85bba8f82d9c2cdee26c058cc5f02e101e1253d8
amine alaoui <omari1amineco@gmail.com>
2023-11-18 19:48:20 +0100

    fix: MainSharedBar component

--- src/components/MainSharedBar.jsx
+++ src/components/MainSharedBar.jsx
@@ -3,7 +3,7 @@ import SharedImagesBar from "./SharedImagesBar";
 
 const MainSharedBar = () => {
   return (
-    <div>
+    <div className="bg-gray-900 p-5">
       <SharedImagesBar />
     </div>
   );
