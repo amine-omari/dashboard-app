d233824e2da415fdf26b7c34ffdd08001c5dad84
amine alaoui <omari1amineco@gmail.com>
2023-11-19 15:53:42 +0100

    fix: ImsSharedContainer component

--- src/components/ImgsSharedContainer.jsx
+++ src/components/ImgsSharedContainer.jsx
@@ -5,7 +5,7 @@ const ImgsSharedContainer = ({ src }) => {
     <img
       src={src}
       alt="Shared Img"
-      className="rounded-xl w-16 h-16"
+      className="rounded-xl w-16 h-16 hover:opacity-80 hover:cursor-pointer"
     />
   );
 };
