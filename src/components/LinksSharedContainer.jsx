8812dbc6f8eba04fab27a36e2d8e15457d92b88c
amine alaoui <omari1amineco@gmail.com>
2023-11-19 16:28:12 +0100

    feat: Add parameter to make LinkSharedConatiner component reusable

--- src/components/LinksSharedContainer.jsx
+++ src/components/LinksSharedContainer.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const LinksSharedContainer = () => {
+const LinksSharedContainer = ({ src, storename, linkcode }) => {
   return (
-    <div>LinksSharedContainer</div>
-  )
-}
+    <div className="flex gap-x-3 items-center">
+      <button className="hover:opacity-80">
+        <img src={src} alt="store img" className="w-16 h-16 rounded-xl" />
+      </button>
+      <div className="flex flex-col gap-y-1 text-sm truncate">
+        <h2 className="text-white">{storename}</h2>
+        <a href="https://dribbble.com/shots/17171600" className="text-gray-500">
+          {linkcode}
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default LinksSharedContainer
+export default LinksSharedContainer;
