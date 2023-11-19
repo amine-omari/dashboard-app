0b2a04bc7044647077c96aad48e61e091df7ea89
amine alaoui <omari1amineco@gmail.com>
2023-11-19 12:21:35 +0100

    feat: import SharedFilesBar component to MainSharedBar component

--- src/components/MainSharedBar.jsx
+++ src/components/MainSharedBar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SharedImagesBar from "./SharedImagesBar";
+import SharedFilesBar from "./SharedFilesBar";
 
 const MainSharedBar = () => {
   return (
     <div className="bg-gray-900 p-5 w-[25%]">
       <SharedImagesBar />
+      <SharedFilesBar />
     </div>
   );
 };
