f8ec06eb951a9f1ad77ecc2f7876cbe0d6f05dfd
amine alaoui <omari1amineco@gmail.com>
2023-11-19 13:28:03 +0100

    feat: Add title and button in SharedFilesBar component

--- src/components/SharedFilesBar.jsx
+++ src/components/SharedFilesBar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const SharedFilesBar = () => {
   return (
-    <div>SharedFilesBar</div>
-  )
-}
+    <div className="space-y-5">
+      <div className="flex justify-between">
+        <h2 className="text-white">Shared Files</h2>
+        <button className="text-sm text-violet-500">Show all</button>
+      </div>
+      <div>
+      </div>
+    </div>
+  );
+};
 
-export default SharedFilesBar
+export default SharedFilesBar;
