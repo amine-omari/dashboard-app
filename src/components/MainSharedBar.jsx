220fe2518e8a488a6606114536710c392e20ffb5
amine alaoui <omari1amineco@gmail.com>
2023-11-19 16:31:53 +0100

    fix: scrollbar of MainSharedBar component

--- src/components/MainSharedBar.jsx
+++ src/components/MainSharedBar.jsx
@@ -5,7 +5,7 @@ import SharedLinksBar from "./SharedLinksBar";
 
 const MainSharedBar = () => {
   return (
-    <div className="bg-gray-900 p-5 w-[25%] space-y-8">
+    <div className="bg-gray-900 p-5 w-[25%] space-y-8 overflow-auto scrollbar-none">
       <SharedImagesBar />
       <SharedFilesBar />
       <SharedLinksBar />
