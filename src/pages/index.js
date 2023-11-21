a8b73e63fd1925c021eb132d55d1394d821bf76f
amine alaoui <omari1amineco@gmail.com>
2023-11-21 14:38:33 +0100

    fix: height of the div in index js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,11 @@ export default function Home() {
     <div className="flex w-full">
       <Navbar />
       <div className="w-full h-screen overflow-hidden">
-        <div className="h-[10%]">
+        <div className="h-[8%]">
           <Topbar />
         </div>
 
-        <div className="flex h-[90%]">
+        <div className="flex h-[92%]">
           <MessagesStoriesBar />
           <MainChatBar />
           <MainSharedBar />
