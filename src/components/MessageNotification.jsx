6e4f9d6762cf76161a7888f9945039a00eb7806d
Omar Ouhra <omarmaroc26@gmail.com>
2023-11-21 12:58:20 +0100

    fix: notification style

--- src/components/MessageNotification.jsx
+++ src/components/MessageNotification.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const MessageNotification = () => {
   return (
-    <p className="text-white bg-violet-500 px-1.5 rounded-full">1</p>
+    <div className="bg-violet-500 w-5 h-5 rounded-full flex items-center justify-center">
+      <p className="text-white text-xs">1</p>
+    </div>
   );
-}
+};
 
-export default MessageNotification
+export default MessageNotification;
