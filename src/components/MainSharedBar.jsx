f584c9acd56827d4de121478ea80d3f3ced05535
Omar Ouhra <omarmaroc26@gmail.com>
2023-11-21 12:58:07 +0100

    fix: MainSharedBar layout

--- src/components/MainSharedBar.jsx
+++ src/components/MainSharedBar.jsx
@@ -5,7 +5,7 @@ import SharedLinksBar from "./SharedLinksBar";
 
 const MainSharedBar = () => {
   return (
-    <div className="bg-gray-900 p-5 w-[25%] space-y-8 overflow-auto scrollbar-none">
+    <div className="bg-gray-900 p-5 w-[25%] space-y-8 overflow-scroll scrollbar-none h-full">
       <SharedImagesBar />
       <SharedFilesBar />
       <SharedLinksBar />
