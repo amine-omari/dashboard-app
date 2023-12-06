77ce0d2b3c2d317d0786c04b8d1476d5f5233a03
amine alaoui <omari1amineco@gmail.com>
2023-12-06 22:50:24 +0100

    rename Button component to NavbarButton component

--- src/components/NavbarButton.jsx
+++ src/components/NavbarButton.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Button = ({icon}) => {
+const NavbarButton = ({icon}) => {
   return (
     <button className="group relative p-6 focus:border-y-[0.2px] focus:border-gray-800/60 focus:bg-gradient-to-r focus:from-violet-950/50 focus:to-gray-900">
       {icon}
@@ -9,4 +9,4 @@ const Button = ({icon}) => {
   );
 };
 
-export default Button;
+export default NavbarButton;
