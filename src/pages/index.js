b794cbe21eb388be3fba4c2a6ba58fa481e93f49
amine alaoui <omari1amineco@gmail.com>
2023-12-07 13:35:17 +0100

    fix: index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,8 @@
-import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Topbar from "@/components/Topbar";
 import MessagesStoriesBar from "@/components/MessagesStoriesBar";
 import MainChatBar from "@/components/MainChatBar";
 import MainSharedBar from "@/components/MainSharedBar";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
