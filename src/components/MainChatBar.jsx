eeab68878a1465f52675fb7aa0609d889cebe5bd
amine alaoui <omari1amineco@gmail.com>
2023-12-07 16:39:30 +0100

    import ChatbarSection1 component to MainChatBar component

--- src/components/MainChatBar.jsx
+++ src/components/MainChatBar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ChatTopbar from "./ChatTopbar";
-import ChatBarSection from "./ChatBarSection";
+import ChatBarSection1 from "./ChatBarSection1";
 import SendMessageBar from "./SendMessageBar";
 
 const MainChatBar = () => {
   return (
     <div className="bg-gray-900 w-[48%] flex flex-col justify-between border-x border-gray-700/60 min-h-[90%]">
       <ChatTopbar />
-      <ChatBarSection />
+      <ChatBarSection1 />
       <SendMessageBar />
     </div>
   );
