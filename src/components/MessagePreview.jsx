7224bd68546fae117ee92d1883049394e58fbc78
amine alaoui <omari1amineco@gmail.com>
2023-12-12 11:53:27 +0100

    fix: MesssagePreview component style

--- src/components/MessagePreview.jsx
+++ src/components/MessagePreview.jsx
@@ -11,23 +11,19 @@ const MessagePreview = ({
   notification,
 }) => {
   return (
-    <button className="p-5 flex items-center space-x-3 w-full border-b border-gray-700/60 hover:bg-gray-700/50">
-      <div className="flex-1">
-        <img
-          src={src}
-          alt="profile pic"
-          className="min-w-12 min-h-12 rounded-full"
-        />
+    <button className="p-5 flex items-center 2xl:justify-between space-x-3 w-full border-b border-gray-700/60 hover:bg-gray-700/50">
+      <div className="flex">
+        <img src={src} alt="profile pic" className="w-12 h-12 rounded-full" />
       </div>
 
       <div className="flex flex-col items-start">
         <p className="text-gray-200">{name}</p>
-        <p className="text-gray-500 text-sm text-left truncate w-[230px] flex-2">
+        <p className="text-gray-500 text-sm text-left truncate w-[230px]">
           {lastMessage}
         </p>
       </div>
 
-      <div className="text-sm mt-2 flex flex-col items-center space-y-0.5 flex-1">
+      <div className="text-sm mt-2 flex flex-col items-center space-y-0.5">
         <time className="text-gray-500 text-xs">{time}</time>
         {seen && <Seen />}
         {notification && <MessageNotification />}
