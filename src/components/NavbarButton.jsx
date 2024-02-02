424f8b7a235975b12a65d9c060a2f883af7f08d2
amine alaoui <omari1amineco@gmail.com>
2024-02-02 16:59:46 +0100

    feat: add onClick function to NavbarButton component

--- src/components/NavbarButton.jsx
+++ src/components/NavbarButton.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-const NavbarButton = ({icon}) => {
+const NavbarButton = ({ icon, onClick }) => {
   return (
-    <button className="group relative p-6 focus:border-y-[0.2px] focus:border-gray-800/60 focus:bg-gradient-to-r focus:from-violet-950/50 focus:to-gray-900">
+    <button
+      onClick={onClick}
+      className="group relative p-6 focus:border-y-[0.2px] focus:border-gray-800/60 focus:bg-gradient-to-r focus:from-violet-950/50 focus:to-gray-900"
+    >
       {icon}
       <div className="absolute left-0 top-0 h-full w-1 group-focus:bg-violet-500/90"></div>
     </button>
