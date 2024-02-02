5e94a3a497ad10fddb5c7165ee3f68b09a4e7ea3
amine alaoui <omari1amineco@gmail.com>
2024-02-02 15:39:20 +0100

    feat: add ActiveUser state to MainChatBar component

--- src/components/MainChatBar.jsx
+++ src/components/MainChatBar.jsx
@@ -3,10 +3,10 @@ import ChatTopbar from "./ChatTopbar";
 import ChatBarSection1 from "./ChatBarSection1";
 import SendMessageBar from "./SendMessageBar";
 
-const MainChatBar = () => {
+const MainChatBar = ({ ActiveUser }) => {
   return (
     <div className="bg-gray-900 w-[48%] flex flex-col justify-between border-x border-gray-700/60 min-h-[90%]">
-      <ChatTopbar />
+      <ChatTopbar ActiveUser={ActiveUser} />
       <ChatBarSection1 />
       <SendMessageBar />
     </div>
