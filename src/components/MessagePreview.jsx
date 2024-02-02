2cecb1054795a866adf96c58df28a62423b027b5
amine alaoui <omari1amineco@gmail.com>
2024-02-02 15:43:40 +0100

    feat: make the buttons dynamic

--- src/components/MessagePreview.jsx
+++ src/components/MessagePreview.jsx
@@ -3,28 +3,32 @@ import React from "react";
 import MessageNotification from "./MessageNotification";
 
 const MessagePreview = ({
+  OnClick,
   src,
-  name,
+  profileName,
   lastMessage,
   time,
   seen,
   notification,
 }) => {
   return (
-    <button className="p-5 flex items-center 2xl:justify-between space-x-3 w-full border-b border-gray-700/60 hover:bg-gray-700/50">
+    <button
+      onClick={OnClick}
+      className="flex w-full items-center space-x-3 border-b border-gray-700/60 p-5 hover:bg-gray-700/50 2xl:justify-between"
+    >
       <div className="flex">
-        <img src={src} alt="profile pic" className="w-12 h-12 rounded-full" />
+        <img src={src} alt="profile pic" className="h-12 w-12 rounded-full" />
       </div>
 
       <div className="flex flex-col items-start">
-        <p className="text-gray-200">{name}</p>
-        <p className="text-gray-500 text-sm text-left truncate w-[230px]">
+        <p className="text-gray-200">{profileName}</p>
+        <p className="w-[230px] truncate text-left text-sm text-gray-500">
           {lastMessage}
         </p>
       </div>
 
-      <div className="text-sm mt-2 flex flex-col items-center space-y-0.5">
-        <time className="text-gray-500 text-xs">{time}</time>
+      <div className="mt-2 flex flex-col items-center space-y-0.5 text-sm">
+        <time className="text-xs text-gray-500">{time}</time>
         {seen && <Seen />}
         {notification && <MessageNotification />}
       </div>
